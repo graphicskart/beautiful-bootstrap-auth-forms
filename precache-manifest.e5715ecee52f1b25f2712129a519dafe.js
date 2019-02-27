c7011b91f6392148323c4f34adcfcbce5c0b3436
graphicskart <graphicskart2018@gmail.com>
2019-02-27 22:40:19 +0530

    Updates

---
+++ precache-manifest.e5715ecee52f1b25f2712129a519dafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d190d263d74a0e85001da428e28d0d4f",
+    "url": "/beautiful-bootstrap-auth-form/static/media/logo.d190d263.png"
+  },
+  {
+    "revision": "e4de480b14fac4cabb76",
+    "url": "/beautiful-bootstrap-auth-form/static/js/runtime~main.e4de480b.js"
+  },
+  {
+    "revision": "cc6a3082d8565e71a0a2",
+    "url": "/beautiful-bootstrap-auth-form/static/js/main.cc6a3082.chunk.js"
+  },
+  {
+    "revision": "4de6ec4f26318c298dad",
+    "url": "/beautiful-bootstrap-auth-form/static/js/2.4de6ec4f.chunk.js"
+  },
+  {
+    "revision": "cc6a3082d8565e71a0a2",
+    "url": "/beautiful-bootstrap-auth-form/static/css/main.a9b692a7.chunk.css"
+  },
+  {
+    "revision": "1e10c087be1186b70b687ec6c843be80",
+    "url": "/beautiful-bootstrap-auth-form/index.html"
+  }
+];
